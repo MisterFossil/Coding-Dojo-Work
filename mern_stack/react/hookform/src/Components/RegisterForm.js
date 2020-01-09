243b6233c449c69e1b6cda1723e173d3190d0c7c
Seas-dev <seastedt.ej@gmail.com>
2020-01-09 16:56:48 -0500

    Adding the More Forms assignment to Github

--- mern_stack/react/hookform/src/Components/RegisterForm.js
+++ mern_stack/react/hookform/src/Components/RegisterForm.js
@@ -5,7 +5,8 @@ const RegisterForm = props => {
         lastName: "",
         email: "",
         password: "",
-        confirmPassword: ""
+        confirmPassword: "",
+        submitted: false
     })
     const onChangeHandler = event => {
         event.preventDefault();
@@ -17,26 +18,36 @@ const RegisterForm = props => {
     const onSubmitHandler = event => {
         event.preventDefault();
         console.log(formState);
+        setFormState({
+            ...formState,
+            submitted: true
+        })
     }
     return (
         <div>
+            <h1>{formState.submitted ? "You have submitted the form!" : "You have not submitted the form yet"}</h1>
             <form onSubmit={onSubmitHandler}>
                 <label>First Name</label>
                 <input type="text" name="firstName" onChange={onChangeHandler}/>
+                <p>{formState.firstName.length < 2 && formState.firstName.length !== 0 ? "First name must be at least 2 characters" : ""}</p>
                 <br/>
                 <label>Last Name</label>
                 <input type="text" name="lastName" onChange={onChangeHandler}/>
+                <p>{formState.lastName.length < 2 && formState.lastName.length !== 0 ? "Last name must be at least 2 characters" : ""}</p>
                 <br/>
                 <label>Email</label>
                 <input type="email" name="email" onChange={onChangeHandler}/>
+                <p>{formState.email.length < 5 && formState.email.length !== 0 ? "Email must be at least 5 characters" : ""}</p>
                 <br/>
                 <label>Password</label>
                 <input type="password" name="password" onChange={onChangeHandler}/>
+                <p>{formState.password.length < 8 && formState.password.length !== 0 ? "Password must be at least 8 characters" : ""}</p>
                 <br/>
                 <label>Confirm Password</label>
                 <input type="password" name="confirmPassword" onChange={onChangeHandler}/>
+                <p>{formState.password === formState.confirmPassword ? "Match" : "Password and Password Confirm must match"}</p>
                 <br/>
-                <input type="submit" />
+                <input type="submit" value="Submit"/>
             </form>
             <br/>
             <h3>Your Form Data</h3>
